7c7164d95ed27aeac79df0149d29d1463a87d2cc
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2025-02-26 13:33:10 +0000

    JS

---
+++ script.js
@@ -0,0 +1,37 @@
+let add = document.querySelector(".add").addEventListener("click", function() {
+    console.log("add");});
+let sub = document.querySelector(".sub").addEventListener("click", function() {
+    console.log("sub");});
+let mult = document.querySelector(".mult").addEventListener("click", function() {
+    console.log("mult");});
+let divide = document.querySelector(".divide").addEventListener("click", function() {
+    console.log("divide");});
+let equal = document.querySelector(".equal").addEventListener("click", function() {
+    console.log("equal");});
+let one = document.querySelector(".one").addEventListener("click", function() {
+    console.log("one");});
+let two = document.querySelector(".two").addEventListener("click", function() {
+    console.log("two");});
+let three = document.querySelector(".three").addEventListener("click", function() {
+    console.log("three");});
+let four = document.querySelector(".four").addEventListener("click", function() {
+    console.log("four");});
+let five = document.querySelector(".five").addEventListener("click", function() {
+    console.log("five");});
+let six = document.querySelector(".six").addEventListener("click", function() {
+    console.log("six");});
+let seven = document.querySelector(".seven").addEventListener("click", function() {
+    console.log("seven");});
+let eight = document.querySelector(".eight").addEventListener("click", function() {
+    console.log("eight");});
+let nine = document.querySelector(".nine").addEventListener("click", function() {
+    console.log("nine");});
+let zero = document.querySelector(".zero").addEventListener("click", function() {
+    console.log("zero");});
+let random = document.querySelector(".random").addEventListener("click", function() {
+    console.log("random");});
+let boom = document.querySelector(".boom").addEventListener("click", function() {
+    console.log("KABOOM");});
+let num1 = 5;
+let num2 = 1;
+let result = num1 + num2;

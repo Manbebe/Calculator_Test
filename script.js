c903c6d7001cefce23c0211693116619ee9eceb8
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2025-02-28 13:32:29 +0000

    Equal sucks

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let currentValue = 0;
 
 let currentOperation = "null";
 
-let display = document.querySelector('.equation')
+
+
+const display = document.querySelector('.equation')
 const add = document.querySelector(".add").addEventListener("click", () => setOperation('+'), {});
 const sub = document.querySelector(".sub").addEventListener("click", () => setOperation('-'), {});
 const mult = document.querySelector(".mult").addEventListener("click", () => setOperation('*'), {});
 const divide = document.querySelector(".divide").addEventListener("click", () => setOperation('/'), {});
-const equal = document.querySelector(".equal").addEventListener("click", () => setOperation('='), {});
+const equal = document.querySelector(".equal").addEventListener("click", () => setOperation(" "), {});
 const one = document.querySelector(".one").addEventListener("click", () => updateValue(1), {});
 const two = document.querySelector(".two").addEventListener("click", () => updateValue(2), {});
 const three = document.querySelector(".three").addEventListener("click", () => updateValue(3), {});
@@ -23,10 +25,12 @@ const zero = document.querySelector(".zero").addEventListener("click", () => upd
 
 function updateValue(value) {
     currentValue = value;
-    display.innerText = currentValue
+    display.innerText += value 
 };
 
 function setOperation(operation) {
     currentOperation = operation;
     console.log("COOLTIME");
+    display.innerText += operation
 };
+
